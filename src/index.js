8ab3a1b30fade38e66883b19cfc0b6e54ca37630
Umayr Shahid <umayrr@hotmail.co.uk>
2015-06-06 02:29:40 +0500

    Breakdown of whole logic into modular blocks.

--- src/index.js
+++ src/index.js
@@ -1,147 +1,54 @@
 /**
  * Author: Umayr Shahid <umayrr@hotmail.com>,
- * Created: 2:11 AM, 27/05/15.
+ * Created: 23:49, 05/06/15.
  */
 
-'use strict';
-
-var request = require('request-promise');
-var cheerio = require('cheerio');
 var Promise = require('bluebird');
+var chalk = require('chalk');
 
-var base = 'https://eatoye.pk/karachi/delivery?&s=rating'; // TODO : Need to move this into constants.
+var fetch = require('./modules/fetch');
 
-var restaurant = require('../lib/repositories/restaurant');
-var menu = require('../lib/repositories/menu');
+var start = Date.now();
+var MAX_PAGE_SIZE = 3; // TODO: Do we need a whole constant file for this? pfft.
 
+// uncomment the line below to fetch a clean copy.
+// clean();
 
-restaurant.sync(true);
-menu.sync(true);
+var promises = [];
 
-request(base).then(function (response) {
-    var $ = parseResponse(response);
-    var items = $('#listing-container > article'); // TODO: See L45.
+for (var i = 0; i <= MAX_PAGE_SIZE; i++) {
+  // Let the race begin.
+  promises.push(fetch.listing(i));
+}
 
-    for (var i = 0; i < items.length; i++) {
-        parseRestaurant(items[i])
-            .then(fetchMenuItems)
-            .spread(function () {
-                console.log('Item inserted.');
-            });
-    }
+// TODO: This doesn't work properly. Need a fix.
+// This should run in the end, but due to the heavy usage of `http request`; promises aren't working as they should.
+// Maybe, I'm wrong but there has to be a way.
 
-});
+Promise.all(promises)
+  .then(function () {
+    console.log(chalk.green('Completed without errors!'));
+    result();
+  }, function () {
+    console.log(chalk.green('Completed with a few errors!'));
+    result();
+  });
 
 /**
- * Fetches menu items for a given restaurant.
- *
- * @param restaurant
+ * Enforces `Sequelize` to drop the tables before inserting anything.
  */
-function fetchMenuItems(restaurant) {
-    request('https://eatoye.pk' + restaurant.url).then(function (response) {
-        var $ = parseResponse(response);
-        var items = $('#single-outlet #menu .tab-content > .tab-pane');
-        for (var i = 0; i < items.length; i++) {
-            parseMenuItem(restaurant.uuid, items[i]);
-        }
-    });
+function clean() {
+  var restaurant = require('../lib/repositories/restaurant');
+  var menu = require('../lib/repositories/menu');
+
+  restaurant.sync(true);
+  menu.sync(true);
 }
 
 /**
- * Wraps the raw response into cheerio object.
- *
- * @param response
- * @returns {object}
+ * Displays `Time Consumed`.
  */
-function parseResponse(response) {
-    return cheerio.load(response, {
-        normalizeWhitespace: true
-    });
+function result() {
+  var end = Date.now();
+  console.log(chalk.yellow('Time Consumed: ') + chalk.bold(end - start + 'ms'));
 }
-
-/**
- * Parses single item in main listing.
- *
- * @param item {object}
- * @returns {Promise}
- */
-function parseRestaurant(item) {
-    var $item = cheerio.load(item);
-
-    // TODO: Below all paths are hard coated. This either needs to be moved into a JSON or separate constants file.
-    var url = $item('.item-pic > a').attr('href');
-    var name = $item('.item-details > .item-title > a').text();
-    var rating = $item('.item-details > .item-title > span').data('rating');
-    var type = $item('.item-details > .item-meta > .item-address').text();
-
-    // TODO: Parse detailed restaurant view.
-
-    return insertRestaurant({
-        url: url,
-        name: name,
-        rating: rating,
-        type: type
-    });
-}
-
-/**
- * Parses single item in the whole menu list for a given restaurant.
- *
- * @param uuid
- * @param item
- */
-function parseMenuItem(uuid, item) {
-    var $parent = cheerio.load(item);
-
-    var type = $parent('h4').text();
-
-    var $children = $parent('.menu-item');
-    var array = [];
-
-    for (var i = 0; i < $children.length; i++) {
-        var $child = cheerio.load($children[i]);
-        var description = $child('.menu-item-name small').text();
-        var name = $child('.menu-item-name').contents()[0].data;
-        var serves = $child('.menu-subitems .menu-subitem .subitem-name').text();
-        var price = $child('.menu-subitems .menu-subitem .subitem-price > span').text();
-
-        array.push(insertMenuItems(
-            {
-                restaurantId: uuid,
-                name: name,
-                type: type,
-                description: description,
-                serves: serves,
-                price: price
-            }
-        ));
-    }
-
-    return Promise.all(array);
-}
-
-/**
- * Inserts a parsed restaurant into database.
- *
- * @param details {object}
- * @returns {Promise}
- */
-function insertRestaurant(details) {
-    return restaurant.insert(details).catch(function (err) {
-        throw new Error(err, details);
-    })
-}
-
-/**
- * Inserts a single menu item for a given restaurant.
- *
- * @param details
- */
-function insertMenuItems(details) {
-    return menu.insert(details).catch(function (err) {
-        console.log(err, details);
-        process.exit(1);
-    });
-}
-
-
