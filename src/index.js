087e4453e56e4d05116411930f4cc70ee4fc0f15
Umayr Shahid <umayrr@hotmail.co.uk>
2015-06-06 02:27:24 +0500

    Complete process for fetching both `restaurant` and `menu`.

--- src/index.js
+++ src/index.js
@@ -7,20 +7,45 @@
 
 var request = require('request-promise');
 var cheerio = require('cheerio');
+var Promise = require('bluebird');
 
 var base = 'https://eatoye.pk/karachi/delivery?&s=rating'; // TODO : Need to move this into constants.
 
 var restaurant = require('../lib/repositories/restaurant');
+var menu = require('../lib/repositories/menu');
+
+
+restaurant.sync(true);
+menu.sync(true);
 
 request(base).then(function (response) {
     var $ = parseResponse(response);
-    var items = $('#listing-container > article'); // TODO: See L43.
+    var items = $('#listing-container > article'); // TODO: See L45.
 
     for (var i = 0; i < items.length; i++) {
-        parseItem(items[i]);
+        parseRestaurant(items[i])
+            .then(fetchMenuItems)
+            .spread(function () {
+                console.log('Item inserted.');
+            });
     }
 
 });
+
+/**
+ * Fetches menu items for a given restaurant.
+ *
+ * @param restaurant
+ */
+function fetchMenuItems(restaurant) {
+    request('https://eatoye.pk' + restaurant.url).then(function (response) {
+        var $ = parseResponse(response);
+        var items = $('#single-outlet #menu .tab-content > .tab-pane');
+        for (var i = 0; i < items.length; i++) {
+            parseMenuItem(restaurant.uuid, items[i]);
+        }
+    });
+}
 
 /**
  * Wraps the raw response into cheerio object.
@@ -38,8 +63,9 @@ function parseResponse(response) {
  * Parses single item in main listing.
  *
  * @param item {object}
+ * @returns {Promise}
  */
-function parseItem(item) {
+function parseRestaurant(item) {
     var $item = cheerio.load(item);
 
     // TODO: Below all paths are hard coated. This either needs to be moved into a JSON or separate constants file.
@@ -50,7 +76,7 @@ function parseItem(item) {
 
     // TODO: Parse detailed restaurant view.
 
-    insertRestaurant({
+    return insertRestaurant({
         url: url,
         name: name,
         rating: rating,
@@ -59,14 +85,63 @@ function parseItem(item) {
 }
 
 /**
+ * Parses single item in the whole menu list for a given restaurant.
+ *
+ * @param uuid
+ * @param item
+ */
+function parseMenuItem(uuid, item) {
+    var $parent = cheerio.load(item);
+
+    var type = $parent('h4').text();
+
+    var $children = $parent('.menu-item');
+    var array = [];
+
+    for (var i = 0; i < $children.length; i++) {
+        var $child = cheerio.load($children[i]);
+        var description = $child('.menu-item-name small').text();
+        var name = $child('.menu-item-name').contents()[0].data;
+        var serves = $child('.menu-subitems .menu-subitem .subitem-name').text();
+        var price = $child('.menu-subitems .menu-subitem .subitem-price > span').text();
+
+        array.push(insertMenuItems(
+            {
+                restaurantId: uuid,
+                name: name,
+                type: type,
+                description: description,
+                serves: serves,
+                price: price
+            }
+        ));
+    }
+
+    return Promise.all(array);
+}
+
+/**
  * Inserts a parsed restaurant into database.
  *
  * @param details {object}
+ * @returns {Promise}
  */
 function insertRestaurant(details) {
-    restaurant.insert(details).then(function () {
-        console.log('woot woot');
-    }).catch(function (err) {
-        console.log(err);
+    return restaurant.insert(details).catch(function (err) {
+        throw new Error(err, details);
     })
 }
+
+/**
+ * Inserts a single menu item for a given restaurant.
+ *
+ * @param details
+ */
+function insertMenuItems(details) {
+    return menu.insert(details).catch(function (err) {
+        console.log(err, details);
+        process.exit(1);
+    });
+}
+
+
